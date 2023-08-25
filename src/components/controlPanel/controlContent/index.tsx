09a3df4ec2b19d1424cdcd067abc8737866044be
Leo <ujfox13@gmail.com>
2023-08-25 23:07:37 +0300

    control content

--- src/components/controlPanel/controlContent/index.tsx
+++ src/components/controlPanel/controlContent/index.tsx
@@ -1,9 +1,31 @@
 import React from 'react';
+import {NavLink} from "react-router-dom";
+import Spacer from "components/common/spacer";
+import {BsPlusCircle} from "react-icons/bs";
 import styles from './ControlContent.module.scss';
+
 const ControlContent = () => {
     return (
         <div className={styles.container}>
-            ControlPanel
+            <NavLink to={"/notes"} className={styles.item}>
+                Заметки
+            </NavLink>
+            <NavLink to={"/consultation"} className={styles.item}>
+                Консультации
+            </NavLink>
+            <NavLink to={"/video"} className={styles.item}>
+                Видео
+            </NavLink>
+            <NavLink to={"/events"} className={styles.item}>
+                Мероприятия
+            </NavLink>
+            <Spacer/>
+            <div className={styles.item}>
+                Новая заметка
+                <div className={styles.icon}>
+                <BsPlusCircle/>
+                </div>
+            </div>
         </div>
     );
 };
