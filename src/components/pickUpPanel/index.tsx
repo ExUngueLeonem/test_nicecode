ca944fab78023fe7c11c945b8a02d7361502ee47
Leo <ujfox13@gmail.com>
2023-08-27 00:55:26 +0300

    wip pickup panel

---
+++ src/components/pickUpPanel/index.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import styles from './PickUpPanel.module.scss';
+const PickUpPanel = () => {
+    return (
+        <div className={styles.container}>
+            <div>
+                count
+            </div>
+            <div>
+                Выбрать
+            </div>
+        </div>
+    );
+};
+
+export default PickUpPanel;
